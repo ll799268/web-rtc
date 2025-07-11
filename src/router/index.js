f8ffa18c2425740948e799acf225c5628780a492
ll799268 <>
2025-07-11 11:34:30 +0800

    feat: 路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,21 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const MettingRTC = () => import('@/views/MettingRTC')
+const P2PRTC = () => import('@/views/P2PRTC')
+const Peer = () => import('@/views/Peer')
 
 const routes = [
   {
     path: '/',
     component: MettingRTC,
+  },
+  {
+    path: '/P2PRTC',
+    component: P2PRTC,
+  },
+  {
+    path: '/Peer',
+    component: Peer,
   }
 ]
 
